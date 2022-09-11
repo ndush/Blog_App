1f3b52408e3396b94746f4c23ecfa8acd3325c80
damaris <mdamarisnduku@gmail.com>
2022-09-12 01:09:09 +0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div className='text-center p-3' style={{ backgroundColor: 'white', width:100+"vw"}}>   
+        © 2022 Copyright: Damaris
+       </div>
+    )
+}
+export default Footer;
