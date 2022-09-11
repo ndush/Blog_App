9725aacb0b1935eded24173bb160f2adddca185f
damaris <mdamarisnduku@gmail.com>
2022-09-12 00:54:14 +0300

    add about content

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="home">
+      {/* <img src={background} class="img-fluid" alt="fireSpot" /> */}
+      <div className="text">
+        <center>
+      <h1> <span style={{fontweight:"bold", color:"purple"}}>Sales</span> <span style={{fontweight:"bold", color:"purple"}}>APP</span></h1>
+        <p className="style">This is a simple app for 
+            users to view different products and their detailed info including
+            the supplier, unit price, quantity remaining from which one 
+            can be able to make an informed desision for purchase
+        </p>
+        </center>
+        </div>
+        
+    </div>
+    
+  );
+}
+
+export default About
