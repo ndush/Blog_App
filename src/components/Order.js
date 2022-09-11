6d45edd43375f65b4e534d9e8d8045f01e3562a3
damaris <mdamarisnduku@gmail.com>
2022-09-12 01:15:31 +0300

    add order cards

---
+++ src/components/Order.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+function Order({quantity,orders}) {
+console.log(quantity);
+  return (
+    
+    <div className="col">
+      <div className="card h-100 create" style={{ width: 18 + "rem" }}>
+        
+        <div className="card-body">
+        
+        </div>
+        {/* <div className="card-footer"> */}
+        {/* <small className="text-muted">{productQuantity}</small> */}
+          {/* <button onClick={handleDelete}>delete</button>
+         */}
+        {/* </div> */}
+      </div>
+     
+    </div>
+  );
+}
+export default Order;
